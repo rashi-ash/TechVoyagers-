e11a8dab58b210ab93ce0f7f7e309ac407edc44c
rashi-ash <rashidav27@gmail.com>
2023-11-07 23:34:43 +0530

    productListing model

--- Stackup/LuxCart/models.py
+++ Stackup/LuxCart/models.py
@@ -1,3 +1,11 @@
+from PIL import Image
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class productlisting(models.Model):
+    image=models.ImageField(upload_to='images',null=True,blank=True)
+    title=models.CharField(max_length=250)
+    rating=models.IntegerField(null=True,blank=True)
+    description=RichTextField(null=True)
+    price=models.IntegerField()
+    discount=models.IntegerField()
--- Stackup/LuxCart/views.py
+++ Stackup/LuxCart/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import productlisting
+
 def home(request):
-    return render(request,'index.html')
+    products_data=productlisting.objects.all()
+    print(products_data)
+    return render(request,'productListing.html',{'products':products_data})
